32be8f7932e16988211a346cf10c64cd65c5b85c
Niraja85 <tripathi.niraja@gmail.com>
2026-05-05 18:11:34 +0200

    Added button to appointment app

--- src/Appointment.jsx
+++ src/Appointment.jsx
@@ -1,9 +1,11 @@
 function Appointment ({patient, date, time}) {
     return (
-        <li>
+        <li style={{ marginBottom: "10px" }}>
             <h3>{patient}</h3>
             <p>{date}</p>
             <p>{time}</p>
+
+            <button onClick={() => alert("Viewing Appointment")}>View</button>
         </li>
     );
 }
